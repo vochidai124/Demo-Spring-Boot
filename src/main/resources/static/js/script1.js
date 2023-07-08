b3a6327f27f8f45c3949a9c85fcecfa0c69439a3
vochidai12@gmail.com <vochidai12@gmail.com>
2023-07-08 21:01:08 +0700

    chỉnh sửa giao diện

--- src/main/resources/static/js/script1.js
+++ src/main/resources/static/js/script1.js
@@ -1,6 +1,7 @@
+// script.js
 let btns = document.querySelectorAll("*[data-modal-btn]");
 
-for (let i = 0; i<btns.length; i++) {
+for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener('click', function () {
     let name = btns[i].getAttribute('data-modal-btn');
     let modal = document.querySelector("[data-modal-window='" + name + "']");
@@ -19,4 +20,20 @@ window.onclick = function (event) {
       modals[i].style.display = "none";
     }
   }
+}
+
+// Thêm mã JavaScript cho bản đồ
+function initMap() {
+  // Tạo đối tượng bản đồ
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 10.762622, lng: 106.660172 }, // Thay đổi tọa độ này thành tọa độ địa điểm của bạn
+    zoom: 15, // Thay đổi mức độ phóng to/buộc nhỏ bản đồ
+  });
+
+  // Thêm đánh dấu vị trí của bạn
+  const marker = new google.maps.Marker({
+    position: { lat: 10.762622, lng: 106.660172 }, // Thay đổi tọa độ này thành tọa độ địa điểm của bạn
+    map: map,
+    title: "Địa chỉ của bạn",
+  });
 }
