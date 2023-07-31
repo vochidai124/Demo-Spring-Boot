0ef3021abf926584fc2d8041bb3c4823a035570b
vochidai12@gmail.com <vochidai12@gmail.com>
2023-07-31 15:07:10 +0700

    edit UI home

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -9,3 +9,22 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove('right-panel-active');
 });
+
+$(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 20,
+      navText: [],
+      autoplay: true,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        768: {
+          items: 2
+        },
+        1000: {
+          items: 2
+        }
+      }
+    });
